e454bca6f986f057f1904ca3fc71b886920a37c2
BCAFAZZO-LAPTOP\roboc <bcafazzo@nbi.company>
2016-11-29 09:56:58 -0500

    Initial project commit.

---
+++ Source/MobileTemplate.Core/App.cs
@@ -0,0 +1,43 @@
+﻿using Xamarin.Forms;
+
+namespace MobileTemplate.Core
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "MobileTemplate",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Source/MobileTemplate.Droid/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using MobileTemplate.Core;
+
+namespace MobileTemplate.Droid
+{
+    [Activity(Label = "MobileTemplate", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
