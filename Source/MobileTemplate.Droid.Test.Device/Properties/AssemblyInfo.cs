62624a3fb20f3f4dd3db9af9e780c4800e4c2b64
BCAFAZZO-LAPTOP\roboc <bcafazzo@nbi.company>
2016-12-08 11:46:36 -0500

    Updating Android Assembly Info for test project.

--- Source/MobileTemplate.Droid.Test.Device/Properties/AssemblyInfo.cs
+++ Source/MobileTemplate.Droid.Test.Device/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MobileTemplate.Droid.Test")]
+[assembly: AssemblyTitle("MobileTemplate.Droid.Test.Device")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MobileTemplate.Droid.Test")]
+[assembly: AssemblyProduct("MobileTemplate.Droid.Test.Device")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
