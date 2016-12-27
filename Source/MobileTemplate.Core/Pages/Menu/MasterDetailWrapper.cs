698f780d0b3629f00ed9865961ba7852ca36dbc5
BCAFAZZO-LAPTOP\roboc <bcafazzo@nbi.company>
2016-12-27 14:42:42 -0500

    Fixing some display issues on iOS.

--- Source/MobileTemplate.Core/Pages/Menu/MasterDetailWrapper.cs
+++ Source/MobileTemplate.Core/Pages/Menu/MasterDetailWrapper.cs
@@ -10,6 +10,10 @@ namespace MobileTemplate.Core.Pages.Menu
             Title = "NBI Mobile Template";
             var mainMenu = new MainMenuPage();
             Master = mainMenu;
+            Device.OnPlatform(() =>
+            {
+                mainMenu.Padding = new Thickness(0, 20, 0, 0); // Need extra top padding on iOS to make room for the status bar.
+            });
 
             var detail = new NavigationPage(new LandingPage()); // Make sure you wrap the detail page in a Navigation Page so you can manipulate the navigation stack.
             Detail = detail;
